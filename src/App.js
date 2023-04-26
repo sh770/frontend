1c966810b91bf1aa47f19afc48b0a38d3b51ddb0
sh <sh770h@gmail.com>
2023-04-26 21:10:35 +0300

    26/04

--- src/App.js
+++ src/App.js
@@ -18,64 +18,69 @@ import SignupScreen from './screen/SignupScreen';
 import PaymentMethodScreen from "./screen/PaymentMethodScreen";
 import PlaceOrderScreen from "./screen/PlaceOrderScreen";
 import OrderScreen from './screen/OrderScreen';
+import OrderHistoryScreen from './screen/OrderHistoryScreen';
+import ProfileScreen from './screen/ProfileScreen';
 
 
 function App() {
 
-const { state, dispatch: ctxDispatch } = useContext(Store);
-const { cart, userInfo } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
 
-const signoutHandler = () => {
-  ctxDispatch({ type: 'USER_SIGNOUT' });
-  localStorage.removeItem('userInfo');
-  localStorage.removeItem('shippingAddress');
-  localStorage.removeItem('paymentMethod');
-  localStorage.removeItem('cartItems');
-  window.location.href = '/signin';
-};
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
+    localStorage.removeItem('cartItems');
+    window.location.href = '/signin';
+  };
 
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
-      <ToastContainer position="top-center" limit={1} />
+        <ToastContainer position="top-center" limit={1} />
         <header >
-          <Navbar bg="dark" variant="dark">
+          <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>החנות בריאקט</Navbar.Brand>
               </LinkContainer>
-              <Nav className='me-auto'>
-                <Link to='/cart' className='nav-link'>
-                  סל הקניות:
-                  {cart.cartItems.length > 0 && (
-                    <Badge pill='danger'>
-                      {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
-                    </Badge>
-                  )}
-                </Link>
-                {userInfo ? (
-                  <NavDropdown title={userInfo?.username} id="basic-nav-dropdown">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>פרופיל משתמש</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to="/orderhistory">
-                      <NavDropdown.Item>הסטוריית קניות</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Divider />
-                    <Link
-                      className="dropdown-item"
-                      to="#signout"
-                      onClick={signoutHandler}
-                    >
-                      התנתק
-                    </Link>
-                  </NavDropdown>
-                ) : (
-                  <Link className="nav-link" to="/signin">
-                    התחברות
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto  w-100  justify-content-end">
+                  <Link to="/cart" className="nav-link">
+                    Cart
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg="danger">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </Badge>
+                    )}
                   </Link>
-                )}
-              </Nav>
+                  {userInfo ? (
+                    <NavDropdown title={userInfo?.username} id="basic-nav-dropdown">
+                      <LinkContainer to="/profile">
+                        <NavDropdown.Item>פרופיל משתמש</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/orderhistory">
+                        <NavDropdown.Item>הסטוריית קניות</NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Divider />
+                      <Link
+                        className="dropdown-item"
+                        to="#signout"
+                        onClick={signoutHandler}
+                      >
+                        התנתק
+                      </Link>
+                    </NavDropdown>
+                  ) : (
+                    <Link className="nav-link" to="/signin">
+                      התחברות
+                    </Link>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </header>
@@ -90,6 +95,8 @@ const signoutHandler = () => {
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/order/:id" element={<OrderScreen />} />
+              <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
               <Route path='/' element={<HomeScreen />}></Route>
             </Routes>
           </Container>
